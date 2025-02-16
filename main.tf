a9667d5716d2dc04d6264b081fd1f9a1c515eb98
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-02-16 23:36:41 +0530

    tf files

--- main.tf
+++ main.tf
@@ -135,7 +135,6 @@ resource "aws_route_table" "database" {
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  #gateway_id = aws_internet_gateway.main.id
   gateway_id = aws_internet_gateway.main.id
 }
 resource "aws_route" "private" {
