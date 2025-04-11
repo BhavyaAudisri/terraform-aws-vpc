c63ec631e6be050978d89e2b127dac106c042ff6
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-11 19:18:45 +0530

    vpc

--- main.tf
+++ main.tf
@@ -26,6 +26,20 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
+# DB Subnet group for RDS
+resource "aws_db_subnet_group" "expense" {
+  name       = local.resource_name
+  subnet_ids = aws_subnet.database[*].id
+
+  tags = merge(
+    var.common_tags,
+    var.db_subnet_group_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
 # expense-dev-public-us-east-1a
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
